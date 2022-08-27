409bae080779186552af3cb28b389e4c5409abe2
Speterius <peet.seres@gmail.com>
2022-08-27 13:51:15 +0200

    Added unit tests to Const class, added doc strings to base_signal.

--- src/signals/sequences.py
+++ src/signals/sequences.py
@@ -4,12 +4,12 @@ from signals.base_signal import Const
 from signals.simple_signals import Signal, Step
 from signals.complex_signals import CosineSmoothedStep
 
-array_like = Union[list | tuple | np.ndarray]
+ArrayLike = Union[list, tuple, np.ndarray]
 
 
-def StepSequence(times: array_like, amplitudes: array_like) -> Signal:
-    """ Generates a sequence of Step signals at the elements of t_i in times with height a_i in amplitudes.
-        The two arguments must have the same length.
+def StepSequence(times: ArrayLike, amplitudes: ArrayLike) -> Signal:
+    """Generates a sequence of Step signals at the elements of t_i in times with height a_i in amplitudes.
+    The two arguments must have the same length.
     """
     assert len(times) == len(amplitudes), "sequence arrays must be of equal length"
 
@@ -20,7 +20,7 @@ def StepSequence(times: array_like, amplitudes: array_like) -> Signal:
         # For the first element the step amplitude is relative to 0.0, otherwise its relative to the previous amplitude
         ampl_prev = 0.0 if idx == 0 else amplitudes[idx - 1]
         ampl_i = amplitudes[idx]
-        delta_ampl = (ampl_i - ampl_prev)
+        delta_ampl = ampl_i - ampl_prev
 
         # Add the signal
         signal += delta_ampl * Step(t_start=t0)
@@ -28,9 +28,11 @@ def StepSequence(times: array_like, amplitudes: array_like) -> Signal:
     return signal
 
 
-def SmoothedStepSequence(times: array_like, amplitudes: array_like, smooth_width: float) -> Signal:
-    """ Generates a sequence of smoothed step signals at the elements of t_i in times with height a_i in amplitudes.
-        The two arguments must have the same length.
+def SmoothedStepSequence(
+    times: ArrayLike, amplitudes: ArrayLike, smooth_width: float
+) -> Signal:
+    """Generates a sequence of smoothed step signals at the elements of t_i in times with height a_i in amplitudes.
+    The two arguments must have the same length.
     """
     assert len(times) == len(amplitudes), "sequence arrays must be of equal length"
 
@@ -42,7 +44,7 @@ def SmoothedStepSequence(times: array_like, amplitudes: array_like, smooth_width
         # For the first element the step amplitude is relative to 0.0, otherwise its relative to the previous amplitude
         ampl_prev = 0.0 if idx == 0 else amplitudes[idx - 1]
         ampl_i = amplitudes[idx]
-        delta_ampl = (ampl_i - ampl_prev)
+        delta_ampl = ampl_i - ampl_prev
 
         # Add the signal
         signal += delta_ampl * CosineSmoothedStep(t_start=t0, width=smooth_width)
